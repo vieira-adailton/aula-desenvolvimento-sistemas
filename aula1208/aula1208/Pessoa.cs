8dc7828ba5d76ad5aa4440d73701904b29069672
unknown <lab1064@etecjk.local>
2025-08-12 22:24:58 -0300

    Aula sobre classes e objetos em C#

---
+++ aula1208/aula1208/Pessoa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula1208
+{
+    class Pessoa
+    {
+        public string nome;
+        public string email;
+        public string telefone;
+
+        public void Cadastrar() { //Função sem retorno
+            Console.WriteLine("Digite o nome do jogador: ");
+            nome = Console.ReadLine();
+            Console.WriteLine("Digite o E-mail do jogador: ");
+            email = Console.ReadLine();
+            Console.WriteLine("Digite o telefone do jogador: ");
+            telefone = Console.ReadLine();
+        }
+
+        public void Exibir()
+        {
+            Console.WriteLine("Nome: {0}", nome);
+            Console.WriteLine("E-mail: {0}", email);
+            Console.WriteLine("Telefone: {0}", telefone);
+        }
+    }
+}
---
+++ aula1208/aula1208/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula1208
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa objJogador; //Cria uma variável na memória
+            Pessoa camisa7;
+            Pessoa camisa10;
+            objJogador = new Pessoa(); //Cria um objeto
+            camisa7 = new Pessoa();
+            camisa10 = new Pessoa();
+
+            objJogador.Cadastrar();
+            camisa10.Cadastrar();
+            camisa7.Cadastrar();
+           
+            objJogador.Exibir();
+            camisa7.Exibir();
+            camisa10.Exibir();
+
+        }
+    }
+}
